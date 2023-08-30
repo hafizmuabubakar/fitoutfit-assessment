bec630356aae10d4a861c46dcbf0ef524f023ea9
Hafiz Abubakar <hafizmuabubakar@gmail.com>
2023-08-30 19:14:43 +0500

    Updated the app.js file

--- app.js
+++ app.js
@@ -4,26 +4,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 const routes = require('./routes');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@mean.01zg0zw.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-
 app.use(express.json());
 
 app.use(routes)
 
-app.listen(port, async () => {
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  console.log(`Server is running on port ${port}`);
-});
+
+
+mongoose.set("strictQuery", false)
+
+mongoose
+  .connect('mongodb+srv://admin:admin@mean.01zg0zw.mongodb.net/')
+.then(() => {
+    app.listen(port, ()=> {
+        console.log('Node API App is running on on port 3000')
+    })
+    console.log('2 ====> Connected to mongoDB')
+}).catch((error)=> {
+    console.log(error)
+})
